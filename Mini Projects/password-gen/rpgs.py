336f8c3461dc3fd2539b2020453ab99b58d8b397
Hatim Sawai <hatimsawai76@gmail.com>
2021-12-08 12:30:26 +0530

    edited rps system
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Mini Projects/password-gen/rpgs.py
+++ Mini Projects/password-gen/rpgs.py
@@ -18,7 +18,7 @@ def pass_save(pswd,a_name):
     '''
     save pswd
     '''
-    with open(f"{a_name}.txt","a") as p:
+    with open(f"D:\\Python-MiniProjects\\Mini Projects\\password-gen\\{a_name}.txt","a") as p:
         p.write(pswd)
     return "Password has been saved successfully!"
 
@@ -41,7 +41,7 @@ def pass_access():
     access a saved pswd
     '''
     ac_fname = input("Which password do you want to access?(enter file name)\n")
-    with open(f"{ac_fname}.txt","r") as f:
+    with open(f"D:\\Python-MiniProjects\\Mini Projects\\password-gen\\{ac_fname}.txt","r") as f:
         f.seek(0)
         ac_pswd = f.readline()
     print(f"Saved password is: {ac_pswd}")
@@ -51,33 +51,37 @@ def pass_delete():
     delete a saved password
     '''
     d_fname = input("Which password do you want to delete?(enter file name)\n")
-    with open(f"{d_fname}.txt","r") as d:
+    with open(f"D:\\Python-MiniProjects\\Mini Projects\\password-gen\\{d_fname}.txt","r") as d:
         d.seek(0)
         d_pswd = d.readline()
     print(f"Saved password is: {d_pswd}")
     dec = input("Are you sure you want to delete this password?(yes/no)\n")
     if dec.lower()=='yes':
-        os.remove(f"{d_fname}.txt")
+        os.remove(f"D:\\Python-MiniProjects\\Mini Projects\\password-gen\\{d_fname}.txt")
         print("Password deleted successfully!")
     else:
         print("Password not deleted!")
 
-while True:
-    opt = input("Do you want to create,access or delete a saved password?(create/access/delete)\n")
-    if opt.lower() == "create":
-        pass_create()
-        replay = input("Do you want to create another password?(yes/no)\n")
-        if replay.lower() == "no":
+def rpgs_start():
+    while True:
+        opt = input("Do you want to create,access or delete a saved password?(create/access/delete)\n")
+        if opt.lower() == "create":
+            pass_create()
+            replay = input("Do you want to create another password?(yes/no)\n")
+            if replay.lower() == "no":
+                break
+        elif opt.lower() == "access":
+            pass_access()
+            replay = input("Do you want to access another password?(yes/no)\n")
+            if replay.lower() == "no":
+                break
+        elif opt.lower() == "delete":
+            pass_delete()
+            replay = input("Do you want to delete another password?(yes/no)\n")
+            if replay.lower() == "no":
+                break
+        else:
             break
-    elif opt.lower() == "access":
-        pass_access()
-        replay = input("Do you want to access another password?(yes/no)\n")
-        if replay.lower() == "no":
-            break
-    elif opt.lower() == "delete":
-        pass_delete()
-        replay = input("Do you want to delete another password?(yes/no)\n")
-        if replay.lower() == "no":
-            break
-    else:
-        break
+
+if __name__ == '__main__':
+    rpgs_start()
