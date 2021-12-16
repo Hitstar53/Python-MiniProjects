0d869062d2987c769181af8ee43170e27f20c6a7
Hatim Sawai <hatimsawai76@gmail.com>
2021-12-16 15:30:15 +0530

    added speedcheck
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Mini Projects/speedcheck.py
+++ Mini Projects/speedcheck.py
@@ -1,3 +1,15 @@
 import speedtest
 s = speedtest.Speedtest()
-print(s.download())
+print("Running SpeedTest.....")
+print("Download: ",s.download()/pow(10,6)," Mbps")
+print("Upload: ",s.upload()/pow(10,6)," Mbps")
+
+best_server = s.get_best_server()
+print('\nBest server:')
+for key,value in best_server.items():
+    print(key,' : ',value)
+
+closest_server = s.get_closest_servers()
+print('\nClosest server:')
+for key,value in closest_server[1].items():
+    print(key,' : ',value)
